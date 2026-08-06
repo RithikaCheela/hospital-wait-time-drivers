11d0529f962ee2a0e39685541d98632e10e4e143
RithikaCheela <rc49479@utexas.edu>
2026-08-05 21:08:10 -0500

    queried more tables

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -4,10 +4,14 @@
 --  Hospital_General_Information
 --  HCAHPS
 
-PRAGMA table_info(Timely_and_Effective_Care);
-PRAGMA table_info(Hospital_General_Information);
-PRAGMA table_info(HCAHPS);
+--PRAGMA table_info(Timely_and_Effective_Care);
+--PRAGMA table_info(Hospital_General_Information);
+--PRAGMA table_info(HCAHPS);
 
-SELECT Facility ID, State, Measure ID, Measure Name, Score, Start Date, End Date
+SELECT Facility_ID, Facility_Name, State, Measure_ID, Measure_Name, Score, Start_Date, End_Date
 FROM Timely_and_Effective_Care 
 LIMIT 10;
+
+SELECT Facility_ID, Facility_Name, State, Hospital_Type, Hospital_Ownership, Emergency_Services, "Hospital overall rating"
+FROM Hospital_General_Information
+LIMIT 10; 
