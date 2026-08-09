66460885c9e32d50faf2539820b9d7ab875f0f00
RithikaCheela <rc49479@utexas.edu>
2026-08-09 18:28:44 -0500

    exploring important variables

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -12,6 +12,16 @@ SELECT Facility_ID, Facility_Name, State, Measure_ID, Measure_Name, Score, Start
 FROM Timely_and_Effective_Care 
 LIMIT 10;
 
+-- time&effective care: OP(OutPatient) measures - 18b, 18c, 20, and 21. different time measures
+
 SELECT Facility_ID, Facility_Name, State, Hospital_Type, Hospital_Ownership, Emergency_Services, "Hospital overall rating"
 FROM Hospital_General_Information
 LIMIT 10; 
+
+SELECT Facility_ID, Facility_Name, State, HCAHPS_Measure_ID, HCAHPS_Question, HCAHPS_Answer_Description, "Patient Survey Star Rating", "HCAHPS Linear Mean Value"
+FROM HCAHPS
+LIMIT 20;
+
+-- HCAHPS dataset has most of the patient satisfaction of the hospital, so star rating and recommendation in the Question column. 
+-- so hcahps can be used to see if there is a correlation between patient satisfaction and hospital wait times. 
+-- can also add in the nurse/doctor communication, cleanliness.... to see if any can have a small correlation with wait times.
--- Information_for_readme.md
+++ Information_for_readme.md
@@ -5,3 +5,11 @@ In your README or write-up, mention this as a known limitation: something like "
 MIT license covers your code. The CMS data itself is separate, public government data is typically public domain in the US (federal data generally isn't copyrightable), but it's worth adding a quick note in your README saying the data comes from CMS's Provider Data Catalog and isn't your own, just for clarity and proper attribution, not a legal requirement, just good practice.
 
 Anyone reviewing your GitHub can still access the data themselves, since it's public CMS data, and you can just link to the source in your README.
+
+
+links for datasets:
+https://data.cms.gov/provider-data/dataset/dgck-syfz#data-dictionary
+
+https://data.cms.gov/provider-data/dataset/xubh-q36u#data-table
+
+https://data.cms.gov/provider-data/dataset/yv7e-xc69
